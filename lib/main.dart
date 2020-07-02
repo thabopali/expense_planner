b8f34e6ffeb6c4c683f88b0922f0cbf7d51dc555
Pali Thabo <tpali@cut.ac.za>
2020-07-02 15:19:36 +0200

    Created Transactions Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transcation.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,15 +16,41 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', 
+        title: 'graphics card', 
+        amount: 69.99, 
+        date: DateTime.now()),
+        Transaction(
+        id: 't2', 
+        title: 'Weekly Groceries', 
+        amount: 16.53, 
+        date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Center(
-        child: Text('Widget Playground!'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text('CHART!'),
+                  elevation: 5,
+                ),
+              ),
+              Card(
+                child: Text('LIST OF TX'),
+                color: Colors.red,
+              )
+            ]));
   }
 }
